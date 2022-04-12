18b52460c6ac52367e3675431cab4c9a23a3b2e6
Rafael Franco <francosprivato@gmail.com>
2022-04-12 18:55:23 -0300

    create dto objects

---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,5 @@
+export * from './restaurant.dto';
+export * from './restaurantReservation.dto';
+export * from './area.dto';
+export * from './tables.dto';
+export * from './weekdays.dto';
---
+++ src/domain/dtos/restaurantReservation.dto.ts
@@ -0,0 +1,46 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsString,
+} from 'class-validator';
+import { WeekdaysDto } from './weekdays.dto';
+
+export class CreateRestaurantReservationDto {
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  readonly restaurantId: string;
+
+  @IsDefined()
+  @IsBoolean()
+  @IsNotEmpty()
+  readonly isActive: boolean;
+
+  @IsDefined()
+  @IsObject()
+  @Type(() => WeekdaysDto)
+  readonly weekdays: WeekdaysDto;
+
+  @IsDefined()
+  @IsNumber()
+  @IsNotEmpty()
+  readonly fromHour: number;
+
+  @IsDefined()
+  @IsNumber()
+  @IsNotEmpty()
+  readonly toHour: number;
+
+  @IsDefined()
+  @IsString({ each: true })
+  readonly inactiveDates: string;
+}
+
+export class UpdateRestaurantReservationDto extends PartialType(
+  CreateRestaurantReservationDto,
+) {}
